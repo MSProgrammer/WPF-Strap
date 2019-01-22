41df37fffa1bebcf47a2ceb5a740b9ac22a231eb
mohammadreza sakhavar <m.sakhavar@hotmail.com>
2019-01-22 09:58:19 +0330

    "Project Add"

---
+++ DictionaryProject/staticValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryProject
+{
+  public static  class staticValue
+    {
+        public static int? FontSizeBtn = 25;
+        public static int? FontHdrSize = 15;
+    }
+}
